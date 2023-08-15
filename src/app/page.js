aae7f1d2c02db59340daf38522a7e0fd7b4d0b82
Abhi4353 <as2271614@gmail.com>
2023-08-15 18:20:24 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+
+import Link from "next/link"
+export default function Home() {
+  return (
+  <>
+ <div>
+  <h1>Home Tab</h1>
+        <Link href="pages/about.js">About</Link>
+        </div>
+  </>
+  )
+}
